0eec9b8bc11b5de02f41d82244e42176e4661425
Ashish Tayade <ashish.tayade4@gmail.com>
2020-05-12 23:38:28 +0530

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,24 @@
+    <a class="templateux-section templateux-cta animsition-link" href="contact.php" data-aos="fade-up">
+      <div class="container-fluid">
+        <div class="cta-inner">
+          <h2><span class="words-1">Start a Project.</span> <span class="words-2">Let’s work together.</span></h2>
+        </div>
+      </div>
+    </a>
+    <!-- END call to action -->
+
+    <footer class="templateux-footer">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6 text-md-left text-center">
+            <p>Copyright &copy;2020 All rights reserved</p>
+          </div>
+          <div class="col-md-6 text-md-right text-center footer-social">
+            <a href="https://www.facebook.com/ashish.tayade.39/" target="_blank" class="p-3"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+            <a href="https://twitter.com/pixpalette" target="_blank" class="p-3"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+            <a href="https://www.linkedin.com/in/ashishtayade/" target="_blank" class="p-3"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+            <a href="https://www.instagram.com/ashish.tayade/" target="_blank" class="p-3"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+          </div>
+        </div>
+      </div>
+    </footer>
---
+++ includes/header.php
@@ -0,0 +1,28 @@
+    <header class="templateux-navbar"  data-aos="fade-down">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-3 col-3"><div class="site-logo"><a href="index.php" class="animsition-link">
+            <img src="images/logo.png" width="50">
+          </a></div></div>
+          <div class="col-sm-9 col-9 text-right">
+
+            <button class="hamburger hamburger--spin toggle-menu ml-auto js-toggle-menu" type="button">
+              <span class="hamburger-box">
+                <span class="hamburger-inner"></span>
+              </span>
+            </button>  
+
+            <nav class="templateux-menu js-templateux-menu" role="navigation">
+              <ul class="list-unstyled">
+                <li class="d-md-none d-block"><a href="index.php" class="animsition-link">Home</a></li>
+                <li><a href="work.php" class="animsition-link">Work</a></li>
+                <li><a href="about.php" class="animsition-link">About</a></li>
+                <li><a href="blog.php" class="animsition-link">Freebies</a></li>
+                <li><a href="photos.php" class="animsition-link">Photos</a></li>
+                <li><a href="contact.php" class="animsition-link">Contact</a></li>
+              </ul>
+            </nav>  
+          </div>
+        </div>
+      </div>
+    </header>
