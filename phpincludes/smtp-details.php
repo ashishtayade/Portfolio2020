5a9bf63a59ca9136027e6220d6e6f15732249edb
Ashish Tayade <ashish.tayade4@gmail.com>
2020-05-12 23:33:41 +0530

    Add files via upload

---
+++ phpincludes/smtp-details.php
@@ -0,0 +1,19 @@
+<?php
+    // $SmtpHost = "mail.dimakhconsultants.com";
+    // $SmtpPort = 25;
+    // $SmtpUserName = "vaibhav-dimakhconsultants";
+    // $SmtpPassword = "Dcpl#5555#";
+    // $FromName = 'DKTE';
+    // $FromEmail = "vaibhav@dimakhconsultants.com";
+    // $RecipientMailId = "vaibhav@dimakhconsultants.com";
+    // $RecipientMailIdCC = "ashish@dimakhconsultants.com";
+
+    $SmtpHost = "smtp.gmail.com";
+    $SmtpPort = 465;
+    $SmtpUserName = "ashish.tayade4@gmail.com";
+    $SmtpPassword = "tryagainbaby";
+    $FromName = 'Ashish Tayade';
+    $FromEmail = "ashish.tayade4@gmail.com";
+    $RecipientMailId = "ashish.tayade4@gmail.com";
+    //$RecipientMailIdCC = "ashish@dimakhconsultants.com";
+?>
